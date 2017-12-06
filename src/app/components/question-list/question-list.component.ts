24441a7d443e0064640fabb8e381aaec5b155af8
Yuri Furusho <yuricfurusho@gmail.com>
2017-12-06 02:11:42 -0200

    8-46 - cards de pergunta e resposta criados

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent implements OnInit {
+  questions: Object[];
 
-  constructor() { }
+  constructor() {
+    this.questions = [
+      {
+        text: 'What is your name?',
+        answer: 'My name is Brad'
+      },
+      {
+        text: 'What is your favorite color?',
+        answer: 'My favorite color is red.'
+      },
+      {
+        text: 'What is your favorite language?',
+        answer: 'My favorite language is Javascript'
+      }
+    ];
+  }
 
   ngOnInit() {
   }
