b0fc35c9651365c38035a497e1ddeacc5b64047a
Yuri Furusho <yuricfurusho@gmail.com>
2017-12-05 23:51:05 -0200

    8-46 - adicao do navbar e questionList como componentes da home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuestionListComponent } from './components/question-list/question-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule
